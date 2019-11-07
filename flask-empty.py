366357b504ee0729280270fdf673f37349a78a29
Ruiz Sanchez Carlos <carlos.ruizsanchez@unamur.be>
2019-11-07 15:31:08 +0000

    some work done

--- flask-empty.py
+++ flask-empty.py
@@ -1,17 +1,30 @@
 from flask import Flask, jsonify, request
-import subprocess
+
+from subprocess import Popen, PIPE
+import re
 
 app = Flask(__name__)
 
+def console(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    out, err = p.communicate()
+    return (p.returncode, out, err)
 
 @app.route('/send/sms', methods=["GET", "POST"])
 def send_sms():
     number = request.values.get("number")
     text = request.values.get("text")
-    return jsonify(
-        msg=subprocess.check_output(['gammu', 'sendsms', 'TEXT', number, '-text', text])
-    )
-
+    code, out, err = console(' '.join(['gammu', 'sendsms', 'TEXT', number, '-text', text]))
+    output = str(out)
+    answer = str(re.search('answer\.\.(.*),', output, re.IGNORECASE).group(1))
+    reference = str(re.search('reference=(\d+).*', output, re.IGNORECASE).group(1))
+    raw = {
+      "code": str(code),
+      "out": output,
+      "err": str(out),
+    }
+    return jsonify(raw=raw, reference=reference, answer=answer)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8888)
+    app.run(host="0.0.0.0", port=8888)
+
