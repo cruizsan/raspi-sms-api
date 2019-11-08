bd009e4aadcba6d1542aad44b7c848e997e88fac
cruizsan <carlos.ruizsanchez@unamur.be>
2019-11-08 09:22:57 +0100

    working on it : split sms

--- raspi_sms_api.py
+++ raspi_sms_api.py
@@ -6,31 +6,41 @@ import shlex
 
 app = Flask(__name__)
 
+
 def console(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
     out, err = p.communicate()
-    return (p.returncode, out, err)
+    return p.returncode, out, err
+
 
 @app.route('/send/sms', methods=["GET", "POST"])
 def send_sms():
     number = request.values.get("number")
-    text = shlex.quote(request.values.get("text"))
-    print(request.values)
-    code, out, err = console(' '.join(['gammu', 'sendsms', 'TEXT', number, '-text', text]))
-    output = str(out)
-    answer = re.search('answer\.\.(.*),', output, re.IGNORECASE)
-    if answer:
-        answer = str(answer.group(1))
-    reference = re.search('reference=(\d+).*', output, re.IGNORECASE)
-    if reference:
-        reference = str(reference.group(1))
-    raw = {
-      "code": str(code),
-      "out": output,
-      "err": str(out),
-    }
-    return jsonify(raw=raw, reference=reference, answer=answer)
+    text = request.values.get("text")
+    n = 152
+    texts = [text[i:i+n] for i in range(0, len(text), n)]
+    result = []
+    for t in texts:
+        code, out, err = console(' '.join(['gammu', 'sendsms', 'TEXT', number, '-text', shlex.quote(t)]))
+        output = str(out)
+        answer = re.search('answer\.\.(.*),', output, re.IGNORECASE)
+        if answer:
+            answer = str(answer.group(1))
+        reference = re.search('reference=(\d+).*', output, re.IGNORECASE)
+        if reference:
+            reference = str(reference.group(1))
+        raw = {
+            "code": str(code),
+            "out": output,
+            "err": str(out),
+        }
+        result.append({
+            "raw": raw,
+            "reference": reference,
+            "answer": answer
+        })
+    return jsonify(result=result)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8888)
-
+    app.run(debug=True, host="0.0.0.0", port=8888)
