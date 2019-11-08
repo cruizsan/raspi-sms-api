21091e38f8a581acd30e9e2a5abc23ff3014f46c
Ruiz Sanchez Carlos <carlos.ruizsanchez@unamur.be>
2019-11-08 08:03:16 +0000

    working on it

--- raspi_sms_api.py
+++ raspi_sms_api.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 
 from subprocess import Popen, PIPE
 import re
+import shlex
 
 app = Flask(__name__)
 
@@ -13,11 +14,16 @@ def console(cmd):
 @app.route('/send/sms', methods=["GET", "POST"])
 def send_sms():
     number = request.values.get("number")
-    text = request.values.get("text")
+    text = shlex.quote(request.values.get("text"))
+    print(request.values)
     code, out, err = console(' '.join(['gammu', 'sendsms', 'TEXT', number, '-text', text]))
     output = str(out)
-    answer = str(re.search('answer\.\.(.*),', output, re.IGNORECASE).group(1))
-    reference = str(re.search('reference=(\d+).*', output, re.IGNORECASE).group(1))
+    answer = re.search('answer\.\.(.*),', output, re.IGNORECASE)
+    if answer:
+        answer = str(answer.group(1))
+    reference = re.search('reference=(\d+).*', output, re.IGNORECASE)
+    if reference:
+        reference = str(reference.group(1))
     raw = {
       "code": str(code),
       "out": output,
